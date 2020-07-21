00a75dfab9df804923cb94fcb095031ff361d07c
Cristian Orozco <cristianorozco1984@gmail.com>
2020-07-21 16:34:02 -0500

    fetch api data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import './App.css'
 
+import Row from './Row'
+import request from './request'
+
 function App() {
-	return <h1>Popcorn</h1>
+	return (
+		<>
+			<h1>Popcorn</h1>
+			<Row title='NEFTLFIX ORIGINAS' fetchUrl={request.netflixOriginals} />
+			<Row title='Trending' fetchUrl={request.trending} />
+		</>
+	)
 }
 
 export default App
