172e7ee83c8c154ebd5111f4569e136677b50bc1
Cristian Orozco <cristianorozco1984@gmail.com>
2020-07-21 22:58:09 -0500

    banner design

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import axios from '../../utils/axios'
+import requests from '../../utils/request'
+import './Banner.css'
 
 const Banner = () => {
+	const [movie, setMovie] = useState([])
+
+	useEffect(() => {
+		async function fetchData() {
+			const request = await axios.get(requests.trending)
+			setMovie(
+				request.data.results[
+					Math.floor(Math.random() * request.data.results.length - 1)
+				]
+			)
+			return request
+		}
+		fetchData()
+	}, [])
+
+	console.log(movie)
+
+	function truncate(str, n) {
+		return str?.length > n ? str.substr(0, n - 1) + '...' : str
+	}
+
 	return (
-		<div>
-			<h1>Banner</h1>
-		</div>
+		<header
+			className='banner'
+			style={{
+				backgroundSize: 'cover',
+				backgroundImage: `url('http://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
+				backgroundPosition: 'center center',
+			}}>
+			<div className='banner_content'>
+				<h1 className='banner_content-title'>
+					{movie?.title || movie?.name || movie?.original_title}
+				</h1>
+				<div className='banner_buttons'>
+					<button className='banner_button'>Play</button>
+					<button className='banner_button'>My List</button>
+				</div>
+				<h1 className='banner_description'>{truncate(movie?.overview, 150)}</h1>
+			</div>
+			<div className='banner_fadeButton'></div>
+		</header>
 	)
 }
 
